45616634a6235c3bed22724208df82e4becc679f
McboyPutra05 <fahmiprismaputradani@gmail.com>
2026-06-20 20:45:25 +0700

    fix: logic be  responsive UI mobile

--- models/status_barang.go
+++ models/status_barang.go
@@ -6,6 +6,7 @@ type StatusBarang struct {
 	ID           uint      `gorm:"primaryKey" json:"id"`
 	CustomerID   uint      `json:"customer_id"` 
 	CustomerName string    `gorm:"type:varchar(150)" json:"customer_name"`
+	Tanggal      string    `gorm:"type:varchar(20)" json:"tanggal"`
 	Code         string    `gorm:"type:varchar(50)" json:"code"`           // cth: "PO 77"
 	JenisBarang  string    `gorm:"type:varchar(150)" json:"jenis_barang"`  // cth: "Celana Pendek Dws"
 	Qty          string    `gorm:"type:varchar(100)" json:"qty"`           // cth: "70 Lusin"
